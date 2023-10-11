2e6c5f5e45128974796e052e07010ee45ea1af2a
sabeehkk <sabeehkadungalloor573@gmail.com>
2023-10-11 20:38:58 +0530

    m

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const app=express()
 
 const session = require('express-session');
 
-
 // app.use(session({ mysessionsec }));
 
 app.use(session({
@@ -17,19 +16,12 @@ app.use(session({
   // You can also configure other session options here if needed
 }));
 
-
-
 //for user  route
 const userRoute=require('./routes/userRoutes')
 app.use('/',userRoute)
 
-
-
-
 const path=require('path')
 const views=path.join(__dirname,'views/user')
-
-
 //for admin routes 
 const adminRoute=require('./routes/adminRoutes')
 app.use('/admin',adminRoute)
@@ -38,11 +30,8 @@ app.use('/admin',adminRoute)
 app.set('view engine','ejs');
 app.set('views', views);
 
-
-
 app.use(express.static(path.join(__dirname,'public')))
 app.use(express.static(path.join(__dirname,'public/admin')))
-
 
 app.get('*', function(req, res, next) {
     const err = new Error('Not Found');
@@ -56,10 +45,8 @@ app.get('*', function(req, res, next) {
     }
     res.statusCode=500
     return res.render('500', { statusCode: res.statusCode });
-
     
   })
-
 
 app.listen(3000,()=>{
     console.log("Server Started");
